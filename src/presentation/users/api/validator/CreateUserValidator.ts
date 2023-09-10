850c4e86b823198de788e95ee8d4eedecac1b21f
Weldisson Araujo <weldisson.araujo@gmail.com>
2023-09-10 15:03:54 -0300

    feat: add more validations in body

--- src/presentation/users/api/validator/CreateUserValidator.ts
+++ src/presentation/users/api/validator/CreateUserValidator.ts
@@ -1,8 +1,17 @@
-import * as Joi from 'joi';
+import * as Joi from "joi";
 
 export const CreateUserValidator = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
-    document: Joi.string()
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  phone: Joi.string()
+    .length(11)
+    .pattern(/^[0-9]+$/)
+    .required(),
+  document: Joi.string()
+    .min(9)
+    .max(11)
+    .pattern(/^[0-9]+$/)
+    .required(),
 });
